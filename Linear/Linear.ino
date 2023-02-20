3225197c4484d92affb4204d5e735a4554ac8e8f
peiykIne08 <64528425+bank17302@users.noreply.github.com>
2023-02-20 12:53:37 +0700

    Update Linear.ino
    fix serial print

--- Linear/Linear.ino
+++ Linear/Linear.ino
@@ -44,6 +44,8 @@ int p_safety_starty;
 bool running = false;
 int wait_2s = 0;
 
+char buff_str[200] = {0}; 
+
 void motor_stop_axis_x() { 
   digitalWrite(PIN_MOTOR_FORWARD_AXIS_X, LOW);
   digitalWrite(PIN_MOTOR_REVERSE_AXIS_X, LOW);
@@ -52,12 +54,12 @@ void motor_stop_axis_x() {
 
 void motor_forward_axis_x(uint8_t speedx) { 
   if(p_safety_stopx == LOW) {
-  	digitalWrite(PIN_MOTOR_FORWARD_AXIS_X, HIGH);
-  	digitalWrite(PIN_MOTOR_REVERSE_AXIS_X, LOW);
-  	ledcWrite(pwm_channel_axis_x, speedx); 
+    	digitalWrite(PIN_MOTOR_FORWARD_AXIS_X, HIGH);
+	digitalWrite(PIN_MOTOR_REVERSE_AXIS_X, LOW);
+	ledcWrite(pwm_channel_axis_x, speedx); 
   }
   else {
-	  motor_stop_axis_x();  
+	motor_stop_axis_x();  
   }	  
 }
 
@@ -68,7 +70,7 @@ void motor_reverse_axis_x(uint8_t speedx) {
   	ledcWrite(pwm_channel_axis_x, speedx);
   }
   else {
-	  motor_stop_axis_x();
+	motor_stop_axis_x();
   }
 }
 
@@ -80,12 +82,12 @@ void motor_stop_axis_y() {
 
 void motor_forward_axis_y(uint8_t speedy) {  
   if(p_safety_stopy == LOW) {
-    digitalWrite(PIN_MOTOR_FORWARD_AXIS_Y, HIGH);
-    digitalWrite(PIN_MOTOR_REVERSE_AXIS_Y, LOW);
-    ledcWrite(pwm_channel_axis_y, speedy);
+	digitalWrite(PIN_MOTOR_FORWARD_AXIS_Y, HIGH);
+	digitalWrite(PIN_MOTOR_REVERSE_AXIS_Y, LOW);
+	ledcWrite(pwm_channel_axis_y, speedy);
   }
   else {
-	  motor_stop_axis_y();
+	motor_stop_axis_y();
   }
 }
 
@@ -96,7 +98,7 @@ void motor_reverse_axis_y(uint8_t speedy) {
   	ledcWrite(pwm_channel_axis_y, speedy);
   }
   else {
-	  motor_stop_axis_y();
+	motor_stop_axis_y();
   }
 }
 
@@ -106,8 +108,8 @@ void encoder(){
   int currentB = digitalRead(PIN_ENCODE_OUT_B_AXIS_X);
   
   if ((lastA == LOW) && (currentA == HIGH)) {
-    if (currentB == LOW) {counter++;} 
-	  else {counter--;}
+	if (currentB == LOW) {counter++;} 
+	else {counter--;}
   }
   
   lastA = currentA;
@@ -145,29 +147,6 @@ void setup() {
   Serial.println(" - start v");
 }
 
-/*
-uint8_t dutyCycle = 0;
-
-void loop() {
-  int p_meun0 = digitalRead(PIN_TEST_MENU0);
-  
-  p_safety_stopx  = digitalRead(PIN_SAFETY_STOP_AXIS_X);
-  p_safety_startx = digitalRead(PIN_SAFETY_START_AXIS_X); 
-  p_safety_stopy  = digitalRead(PIN_SAFETY_STOP_AXIS_Y);
-  p_safety_starty = digitalRead(PIN_SAFETY_START_AXIS_Y);
-
-  //motor_reverse_axis_x(200);
-  motor_reverse_axis_y(255);
-  delay(2000);
-
-  //motor_forward_axis_x(200);
-  motor_forward_axis_y(255);
-  delay(2000);
-
-  Serial.println(" safety:" + String(p_safety_stopx) + String(p_safety_startx) + String(p_safety_stopy) + String(p_safety_starty) + " SW:" +  String(p_meun0) + " counter:" + String(counter / 191) + " dutyCycle:" + String(dutyCycle));
-  //delay(100);
-}*/
-  
 void loop() {
 	double p_count = counter / 191;
   
@@ -179,15 +158,19 @@ void loop() {
 	p_safety_starty = digitalRead(PIN_SAFETY_START_AXIS_Y);
 	
 	if (p_meun0 == LOW and running == false) {
-		//step == 2 END
-		//step == 1 forward_axis_y
+		/*
+		*	step == 2 is END
+		*	step == 1 is forward_axis_y
+		*	step == other is the distance
+		*/
+		
 		step[0] = 10;
 		step[1] = 1;
 		step[2] = 1;
-    step[3] = 30;
-    step[4] = 1;
-    step[5] = 50;
-    step[6] = 1;
+		step[3] = 30;
+		step[4] = 1;
+		step[5] = 50;
+		step[6] = 1;
 		step[7] = 2;
 		
 		ix_status = 20;
@@ -243,7 +226,7 @@ void loop() {
 				
 				running = false;
 				ix_status = 0;
-        ix_step = 0;
+        			ix_step = 0;
 			}
 			else {
 				motor_reverse_axis_x(150);
@@ -251,14 +234,11 @@ void loop() {
 			break;
 			
 		default:
-			// statements
 			break;
 	}
 
-	// + "" + String()
-	Serial.println(" - ix_status:" + String(ix_status) + " ix_step:" + String(ix_step) + " running:" + String(running) + 
-	               " safety:" + String(p_safety_stopx)+ String(p_safety_startx) + String(p_safety_stopy) + String(p_safety_starty) + 
-	               " p_count:" + String(p_count));
-	
+	sprintf(buff_str, " - ix_status:%d ix_step:%d running:%d p_count:%f \n", ix_status, ix_step, running, p_count);
+	Serial.print(buff_str);
+
 	delay(10);
 }
